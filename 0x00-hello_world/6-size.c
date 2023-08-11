2aa5ad111747f2532bdd8f545485dd87ab5f3d94
GLICON <atkinsonbayo@example.com>
2023-08-11 14:01:26 -0700

    Done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,14 @@
 
 int main(void)
 {
-puts("\Programming is like building a multilingual puzzle");
-return (0);
+     char a;
+     char b;
+     long int c;
+     long long int d;
+     float f;
+printf("Size of a char : %lu bytes(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int : %lu bytes(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int : %lu bytes(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
 }
