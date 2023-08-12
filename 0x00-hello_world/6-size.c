6d818c2175a84b4f5c37cca38e7e87fcd076fb4b
GLICON <atkinsonbayo@example.com>
2023-08-11 19:04:23 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ long int z;
 long long int t;
 float b;
 printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(x));
-printf("Size of a int: %d byte(s)\n", (unsigned long)sizeof(y));
+printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(y));
 printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(z));
 printf("Size of a long long int: %d byte(s)\n", (unsigned long)sizeof(t));
 printf("Size of a float: %d bytes(s)\n", (unsigned long)sizeof(b));
