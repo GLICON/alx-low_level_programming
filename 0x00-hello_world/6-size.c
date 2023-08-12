5d312f81901c01230a6f9275ee884972bf2b5b82
GLICON <atkinsonbayo@example.com>
2023-08-11 18:03:06 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,16 +8,16 @@
 
 int main(void)
 {
-	char x;
-	char y;
-	long int z;
-	long long int i;
-	float b;
+	char a;
+	char b;
+	long int c;
+	long long int d;
+	float f;
 
-printf("Size of a char : %lu bytes(s)\n", (unsigned long)sizeof(x));
-printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(y));
-printf("Size of a long int : %lu bytes(s)\n", (unsigned long)sizeof(z));
-printf("Size of a long long int : %lu bytes(s)\n", (unsigned long)sizeof(i));
-printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(b));
+printf("Size of a char : %lu bytes(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int : %lu bytes(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int : %lu bytes(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
