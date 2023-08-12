aaf7e01aa039a71edf9d431462680eed72a3be3f
GLICON <atkinsonbayo@example.com>
2023-08-11 18:42:44 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - a program that prints a line to the standard error
+ * main - A program that prints a line to the std error
  *
- * Return 1 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
