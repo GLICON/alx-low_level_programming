226ca0b9a9ba41c60e178f1fc613c5ca1da1b28d
GLICON <atkinsonbayo@example.com>
2023-08-13 10:41:31 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 i = n % 10;
 	if (i > 5)
-		printf("Last digit of %d is %d and is greater 5\n", n, i);
+		printf("Last digit of %d is %d and is greater than 5\n", n, i);
 	if (i == 0)
 		printf("Last digit of %d is %d and is 0\n", n, i);
 	if (i < 6 && i != 0)
