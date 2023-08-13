f430df6672f93425286130fe95d0675f75c0a565
GLICON <atkinsonbayo@example.com>
2023-08-13 13:18:41 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,20 +7,20 @@
 
 int main(void)
 {
-        int lowercase = 'a';
+int lowercase = 'a';
 
-        while (lowercase <= 'z')
-        {
-                if (lowercase == 'e' || lowercase == 'q')
-                {
-                        lowercase += 1;
-                }
-                else
-                {
-                        putchar(lowercase);
-                        lowercase += 1;
-                }
-        }
-        putchar ('\n');
-        return (0);
+	while (lowercase <= 'z')
+	{
+		if (lowercase == 'e' || lowercase == 'q')
+		{
+			lowercase += 1
+		}
+		else
+		{
+			putchar(lowercase);
+			lowercase += 1;
+		}
+	}
+	putchar ('\n');
+	return (0);
 }
