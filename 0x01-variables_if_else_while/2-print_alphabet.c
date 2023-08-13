50b6b4501d4c206a81cd1ceb773ff13ed4eb6fae
GLICON <atkinsonbayo@example.com>
2023-08-13 10:55:01 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the alphabet in lowercase
+ *
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+	char w;
+
+	w = "a";
+	while
+		(w <= "z")
+		{
+			putchar (w);
+			w++;
+		}
+	putchar ("\n");
+	return (0);
+}
