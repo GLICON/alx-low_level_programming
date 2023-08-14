d0db73f1562dca39389f8523f9f6a3caadc17d80
GLICON <atkinsonbayo@example.com>
2023-08-14 03:06:29 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the alphabet in lowercase
+ *
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+	char w;
+for (w = 'z'; w <= 'a'; w--)
+{
+	putchar(w);
+}
+putchar('\n');
+return (0);
+}
