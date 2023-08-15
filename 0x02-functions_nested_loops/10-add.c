9fc069286ba26ce8eb842af364e20540e00e5aea
GLICON <atkinsonbayo@example.com>
2023-08-15 15:16:08 -0700

    7-print_last_digitwq.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * add - Entry
+ * Return: 0 Always (Success)
+ */
+int add(int a, int b)
+{
+	c = a + b;
+return (c);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry
+ * Return: 0 Always (Success)
+ */
+void jack_bauer(void)
+{
+int x;
+int y;
+int z;
+int w;
+        for (w = 0; w <= 2; w++)
+        {
+		for (x = 0; x <= 3; x++)
+
+                i = -i;
+        }
+        n = (i % 10);
+        if (n < 0)
+        {
+                n = -n;
+        }
+        _putchar(n + '0');
+        return (n);
+}
