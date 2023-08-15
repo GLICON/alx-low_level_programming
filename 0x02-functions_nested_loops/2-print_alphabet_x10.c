0814ac7d91c1ae15241be00b6bbde0c2f61b9504
GLICON <atkinsonbayo@example.com>
2023-08-15 12:57:24 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry
+ */
+
+void print_alphabet_x10(void)
+{
+char x;
+char y;
+	for (x = 0; x <= 10; x++)
+	{
+		_putchar('\n');
+			for (y = 'a'; y <= 'z'; y++)
+			{
+				_putchar(x);
+			}
+	}
+}
