35b963f18c3ded2fc029435ce4ecda92b4830c50
GLICON <atkinsonbayo@example.com>
2023-08-14 19:18:03 -0700

    Done"

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i = '0';
+int n = '0';
+int k = '0';
+
+while (i <= '9')
+{
+	while (n <= '9')
+	{
+		while (k <= '9")
+		{
+			if (i != n != k && i < n < k)
+			{
+			       putchar(i);
+			       putchar(n);
+			       putchar(k);
+			       if (i == '7' && n == '8' && k =='9')
+			       {
+				       putchar('\n');
+			       }
+			       else
+			       {
+				       putchar(',');
+				       putchar(' ');
+			       }
+		       }
+	       }
+	       k++;
+	}
+	k = '0';
+	n++;
+}
+n = '0';
+i++;
+}
+return (0);
+}
