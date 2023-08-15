0fd307c81da97b562d04a18b72644b7ceae89863
GLICON <atkinsonbayo@example.com>
2023-08-15 14:40:11 -0700

    7-print_last_digitwq.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry
+ *@i: the integer used to check
+ * Return: 0 Always (Success)
+ */
+
+int print_last_digit(int i)
+{
+int n;
+	n = _putchar('0' + (i % 10);
+return (n);
+}
