baeba4e0f2fa41ae271b6659694e6ffdea7cbdff
GLICON <atkinsonbayo@example.com>
2023-08-17 13:59:03 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * _isupper - A function that checks for uppercase character
+ * @c: charatere used
+ * Return: 1 if uppercase, otherwise 0
+ */
+
+int _isupper(int c)
+{
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
