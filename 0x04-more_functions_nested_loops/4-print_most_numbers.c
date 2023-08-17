a85d7039923a44ca217cd93283a056da23eb5a11
GLICON <atkinsonbayo@example.com>
2023-08-17 16:08:18 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,18 +2,17 @@
 
 /**
  * print_numbers - To print 0-9
+ *
  * Return: void
  */
 
 void print_numbers(void)
 {
 char i;
+	
 	for (i = '0'; i <= '9'; i++)
 	{
-		if (i == '2' || i == '4')
-		{
-		}
-		else
+		if (!(i == '2' || i == '4'))
 		{
 			_putchar(i);
 		}
