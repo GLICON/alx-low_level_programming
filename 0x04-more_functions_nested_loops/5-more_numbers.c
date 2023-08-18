7f94df2761c0c420b421b86cf84b12698b3ca969
GLICON <atkinsonbayo>
2023-08-18 06:00:39 -0700

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,17 +8,17 @@
 
 void more_numbers(void)
 {
-int i;
-int j;
-	for (i = 0; i < 10; i++)
+int x;
+int y;
+	for (x = 0; x < 10; x++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (y = 0; y <= 14; y++)
 		{
-			if (j >= 10)
+			if (y >= 10)
 			{
 				_putchar('1');
+				_putchar('0' + (y % 10));
 			}
-			_putchar('0' + (j % 10));
 		}
 		_putchar('\n');
 	}
