2b793a5899c7fabb20edec72ac77a7ecdbc27dd5
GLICON <atkinsonbayo>
2023-08-18 19:59:32 -0700

    0-main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - To determine if a number is positive or negative or zero
+ * 0: number to be checked
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+	return;
+}
