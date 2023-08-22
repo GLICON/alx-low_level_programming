9d7690fb9f2d4747432b1437d313fb00568be5f4
GLICON <atkinsonbayo>
2023-08-22 11:34:32 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string, in reverse
+ * @s: string
+ * return: 0
+ */
+
+void print_rev(char *s)
+{
+	int longi = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (i = longi; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
