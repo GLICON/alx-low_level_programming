6bc722d55c8530ef1f293754127ba8e815b9be1f
GLICON <atkinsonbayo>
2023-08-24 01:08:33 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * cap_string - A function that capitalizes all words of a string.
+ * @str: The inputed string.
+ *
+ * Return: A pointer to the changed string.
+ */
+
+char *cap_string(char *str)
+{
+	int ind = 0;
+
+	while (str[ind])
+	{
+		while (!(str[ind] >= 'a' && str[ind] <= 'z'))
+			ind++;
+
+		if (str[ind - 1] == ' ' ||
+		    str[ind - 1] == '\t' ||
+		    str[ind - 1] == '\n' ||
+		    str[ind - 1] == ',' ||
+		    str[ind - 1] == ';' ||
+		    str[ind - 1] == '.' ||
+		    str[ind - 1] == '!' ||
+		    str[ind - 1] == '?' ||
+		    str[ind - 1] == '"' ||
+		    str[ind - 1] == '(' ||
+		    str[ind - 1] == ')' ||
+		    str[ind - 1] == '{' ||
+		    str[ind - 1] == '}' ||
+		    index == 0)
+			str[ind] -= 32;
+
+		ind++;
+	}
+
+	return (str);
+}
+
