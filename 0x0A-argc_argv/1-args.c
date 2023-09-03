c274c9d21ab3a1d3d38ada30a11631bf07b327eb
GLICON <atkinsonbayo@example.com>
2023-09-03 13:19:02 +0000

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - to print the number of arguments passed to the program
+ * @argc: arguments count
+ * @argv: arguments array
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
