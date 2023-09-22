f224b07e18d1b44c54143c8f32c5ca842b26a5b2
GLICON <atkinsonbayo@example.com>
2023-09-22 15:43:06 -0700

    DONE

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,19 +3,19 @@
 
 /**
  * list_len - to return the number of elements in a linked list
- * @h: pointer to list_t 
+ * @h: pointer to list_t
  *
  * Return: x
  */
-size_t list_len (const list_t *h)
+size_t list_len(const list_t *h)
 {
-	size_t x = 0;
+size_t x = 0;
 
-	while(h)
-	{
-		x++;
-		h = h->next;
-	}
-	return (x);
+while (h)
+{
+	x++;
+	h = h->next;
+}
+return (x);
 }
 
