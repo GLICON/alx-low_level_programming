c2a63c3761e9cc7fa13768d61f94604d95ddd66c
GLICON <atkinsonbayo@example.com>
2023-09-27 14:56:59 -0700

    done

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,22 +1,22 @@
-#include <stdio.h>
 #include "lists.h"
-
+#include <stdio.h>
 /**
- * print_listint - to print all the elements of listint_t
- * @h: linked list
- *
- * Return: number of nodes in the list
- */
-
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
 size_t print_listint(const listint_t *h)
 {
-size_t num = 0;
+	size_t num = 0;
 
-while (h)
-{
-	printf("%d/n", h->n);
-	num++;
-	h = h->next;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+
+	return (num);
 }
-return (num);
-}
+
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,12 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stdlib.h>
-
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * *@n: integer
  * @next: points to the next node
- * 
  * Description: singly linked list node structure
  */
 
