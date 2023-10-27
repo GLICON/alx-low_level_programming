5da3efb3858f8dd25a3f1d45a19247d22eb9fb96
GLICON <atkinsonbayo@example.com>
2023-10-27 14:03:27 -0700

    dne

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - program that prints the number of arguments passed into it.
+ * main - program that prints its name, followed by a new line.
  * @argc: argument count
  * @argv: argument vector
  *
