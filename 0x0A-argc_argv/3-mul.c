2b145f4ab1efbbc69325c9b5991cd9e275a93b53
GLICON <atkinsonbayo@example.com>
2023-10-29 14:06:41 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,70 +5,70 @@
  * stoi - converts string to integer
  * @s: the string
  *
- * Return: 0 always
+ * Return: the converted integer
  */
 
 int stoi(char *s)
 {
-	int i, d, n, len, f, digit = 0;
+int i, d, n, len, f, digit = 0;
 
-	while (s[len] != '\0')
+while (s[len] != '\0')
+{
+	len++;
+}
+while (i < len && f == 0)
+{
+	if (s[i] == '-')
 	{
-		len++;
+		++d;
 	}
-	while (i < len && f == 0)
+	if (s[i] >= '0' && s[i] <= '9')
 	{
-		if (s[i] == '-')
+		digit = s[i] - '0';
+		if (d % 2)
 		{
-			++d;
+			digit = -digit;
 		}
-		if (s[i] >= '0' && s[i] <= '9')
+		n = n * 10 + digit;
+		f = 1;
+		if (s[i + 1] < '0' || s[i + 1] > '9')
 		{
-			digit = s[i] - '0',
-			      if (d % 2)
-			      {
-				      digit = -digit;
-			      }
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i+1] < '0' || s[i + 1] > '9')
-			{
-				break;
-			}
-			f = 0;
+			break;
 		}
-		i++;
+		f = 0;
 	}
-	if (f == 0)
-	{
-		return (0);
-	}
-	return (n);
+	i++;
+}
+if (f == 0)
+{
+	return (0);
+}
+return (n);
 }
 
+
 /**
- * main - program to multiply two numbers
+ * main - program that multiplies two numbers
  * @argc: argument count
  * @argv: argument vector
  *
- * Return: 0 always
+ * Return: 0 always but 1(Error))
  */
 
 int main(int argc, char *argv[])
 {
-int mul, n1, n2;
+int res, n1, n2;
 
-if (argc > 3||argc < 3)
+if (argc < 3 || argc > 3)
 {
 	printf("Error\n");
 	return (1);
 }
-
 n1 = stoi(argv[1]);
 n2 = stoi(argv[2]);
-mul = n1 *n2;
+res = n1 *n2;
 
-printf("%d\n", mul);
-
+printf("%d\n", res);
 return (0);
 }
+
