0612cb63d6c84edbbdb0a4dd8d9f977595dc177b
GLICON <atkinsonbayo@example.com>
2023-11-01 08:53:45 -0700

    libmy.a

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_abs - Entry
+ *@i: the integer used to check
+ * Return: 0 Always (Success)
+ */
+
+int _abs(int i)
+{
+	if (i > 0)
+	{
+		return (i);
+	}
+	return (-i);
+}
