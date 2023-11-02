7a7a069e11957f57eebb62acdc11f9d4b85ba4d1
GLICON <atkinsonbayo@example.com>
2023-11-02 09:32:10 -0700

    0-malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ *  @b: memory size
+ *
+ *  Return: new memory pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *result;
+
+result = malloc(b);
+
+if (result  == NULL)
+{
+	exit(98);
+}
+return (result);
+}
