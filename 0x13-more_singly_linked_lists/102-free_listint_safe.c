c263486bb2582b6e912804c4a95244a4e6449505
GLICON <atkinsonbayo01@example.com>
2023-11-21 02:13:46 -0800

    TASK 102

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a linked list
+ * @h: ptr to the first node in the linked list
+ *
+ * Return: no of elements in the freed list
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+size_t l = 0;
+int diff;
+listint_t *opp;
+
+if (!h || !*h)
+	return (0);
+
+while (*h)
+{
+	diff = *h - (*h)->next;
+	if (diff > 0)
+	{
+		opp = (*h)->next;
+		free(*h);
+		*h = opp;
+		l++;
+	}
+	else
+	{
+		free(*h);
+		*h = NULL;
+		l++;
+		break;
+	}
+}
+
+*h = NULL;
+
+return (l);
+}
