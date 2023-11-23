cd6309853334a3f4a23d20925484f838fe2213f3
GLICON <atkinsonbayo01@example.com>
2023-11-23 10:55:11 -0800

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function converts a binary number to an unsigned int.
+ * @b: ptr to string containing a binary number
+ *
+ * Return: the converted number, or 0 if error
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+int a;
+unsigned int n;
+
+n = 0;
+if (!b)
+	return (0);
+
+for (a = 0; b[a] != '\0'; a++)
+{
+	if (b[a] != '0' && b[a] != '1')
+		return (0);
+}
+for (a = 0; b[a] != '\0'; a++)
+{
+	n <<= 1;
+	if (b[a] == '1')
+		n += 1;
+}
+return (n);
+}
